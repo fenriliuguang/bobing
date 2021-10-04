2dfb1487a59bbded60e85dfaccf476986fcfeb9b
fenriliuguang <1415854799@qq.com>
2021-10-04 10:17:54 +0800

    test : 登录接口测试

---
+++ pages/login/login.js
@@ -0,0 +1,112 @@
+// pages/login/login.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        isdone: false,
+        userInfo: null,
+        avatarUrl: "../../static/image/wechat.png",
+        isLogin: false
+    },
+
+    onts: function(e){
+        this.setData({
+            isdone: true
+        })
+    }, 
+
+    outs: function(){
+        this.setData({
+            isdone:false
+        })
+    },
+
+    canLogin: function () {
+        if (!this.data.isLogin){
+            wx.getUserProfile({
+                desc : "用于完善个人信息",
+                success: (res) => {
+                    this.setData({
+                        userInfo: res.userInfo,
+                        avatarUrl :res.userInfo.avatarUrl,
+                        isLogin: true
+                    })
+                }
+            })
+
+            setTimeout(()=>{
+                wx.login({
+                    success: (res) => {
+                        if (res.code) {
+                            wx.request({
+                                // 登录接口
+                                url: 'http://localhost:8080/login',
+                                data: {
+                                    code: res.code
+                                }
+                            })
+                        }
+                    }
+                })
+            },1000)
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        wx.hideHomeButton()
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pages/openScreen/openScreen.js
@@ -0,0 +1,15 @@
+// pages/openScreen/openScreen.js
+Page({
+    /**
+     * 生命周期函数--监听页面显示
+     * 开屏页自动跳转
+     */
+    onShow: function () {
+        wx.hideHomeButton()
+        setTimeout(function(){
+            wx.redirectTo({
+              url: '/pages/login/login',
+            })
+        },3000)
+    }
+})
